169932dfc61aa7dc78a9d846d414a1bf61bb2a28
Chris Kirkham <chrisjameskirkham@gmail.com>
2011-08-25 17:39:54 +0100

    Implemented 12-bit CRC functionality

---
+++ include/crc12.h
@@ -0,0 +1,11 @@
+#ifndef _KOKI_CRC12_H_
+#define _KOKI_CRC12_H_
+
+/**
+ * @file  crc12.h
+ * @brief Header file for 12-bit CRC calculation
+ */
+
+uint16_t koki_crc12(uint8_t input);
+
+#endif /* _KOKI_CRC12_H_ */
---
+++ src/crc12.c
@@ -0,0 +1,90 @@
+
+/**
+ * @file  crc12.c
+ * @brief Implementation for 12-bit CRC
+ */
+
+#include <stdint.h>
+
+#define WIDTH 12
+#define SEED  0
+#define XOR_MASK 0
+
+#define POLY_LEN 6
+static uint8_t polynomial[] = {12, 11, 3, 2, 1, 0};
+
+/**
+ * @brief reflects (i.e. reverses) the bits in the given input \c num
+ *
+ * @param num    the input to reverese/reflect
+ * @param width  the width of the input in bits (e.g. if reversing 7
+ *               bits, use 7)
+ * @return       the input \c num, reversed
+ */
+static uint16_t reflect(uint16_t num, uint8_t width)
+{
+
+	uint16_t ret = 0;
+
+	for (int i=0; i<width; i++){
+		ret |= ((num >> i) & 0x1) << (width-1-i);
+	}
+
+	return ret;
+
+}
+
+
+
+/**
+ * @brief computes the 12-bit CRC on an 8-bit input
+ *
+ * Based on Ray Burr's CrcMoose.py (included).  Please see CrcMoose.py
+ * for MIT license, copyright, etc... Thanks Ray!
+ *
+ * Note that this fonction only operates on a single value.
+ *
+ * @param input  the number/char/etc... to CRC
+ * @return       the 12-bit CRC of \c input
+ */
+uint16_t koki_crc12(uint8_t input)
+{
+
+	uint16_t value, bit, out_bit;
+	uint16_t bit_mask, poly_mask, word;
+	uint16_t /* in_bit_mask,*/ out_bit_mask;
+
+	bit_mask = (1 << WIDTH) - 1;
+
+	word = 0;
+	for (int i=0; i<POLY_LEN; i++)
+		word |= (1 << polynomial[i]);
+
+	poly_mask = word & bit_mask;
+
+	// lsb first
+	poly_mask = reflect(poly_mask, WIDTH);
+
+	//in_bit_mask = 1 << (WIDTH - 1);
+	out_bit_mask = 1;
+
+	value = 0;
+
+
+	// take bit
+	for (uint8_t i=0; i<8; i++){
+
+		bit = ((input & (1 << i)) >> i) & 0x1;
+
+		out_bit = ((value & out_bit_mask) != 0) & (0x1);
+		value = value >> 1;
+		value = value & bit_mask;
+
+		if (out_bit ^ (bit & 0x1))
+			value ^= poly_mask;
+
+	}
+
+	return (value ^ XOR_MASK) & 0xFFF;
+
+}
