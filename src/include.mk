88b39a7f663e738dea53337c5a0cad694cb5d736
Robert Spanton <rspanton@zepler.net>
2011-08-26 02:56:17 +0100

    Build header-dependency rules for C files automatically
    This means that when header files are changed, things are rebuilt.

--- src/include.mk
+++ src/include.mk
@@ -3,6 +3,14 @@ SO_OBJS := $(addprefix src/,labelling.o contour.o quad.o pca.o marker.o \
 
 CLEAN += $(SO_OBJS) lib
 
+include src/depend
+
+src/depend: src/*.c
+	rm -f src/depend
+	for file in src/*.c; do \
+		$(CC) $(CFLAGS) -MM $$file -o - >> $@ ; \
+	done ;
+
 solib: lib/libkoki.so
 
 lib/libkoki.so: $(SO_OBJS)
