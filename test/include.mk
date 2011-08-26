088a2e5f1594e2b48ed8e854a834042fcf3436fe
Robert Spanton <rspanton@zepler.net>
2011-08-26 03:08:25 +0100

    Build header dependencies for test directory as well

--- test/include.mk
+++ test/include.mk
@@ -2,6 +2,14 @@ EXAMPLE_BINS := $(addprefix test/,example realtime debug_img marker_info)
 
 CLEAN += $(EXAMPLE_BINS)
 
+include test/depend
+
+test/depend: src/*.c
+	rm -f test/depend
+	for file in src/*.c; do \
+		$(CC) $(CFLAGS) -MM $$file -o - >> $@ ; \
+	done ;
+
 examples: $(EXAMPLE_BINS)
 
 $(EXAMPLE_BINS): % : %.o lib/libkoki.so
