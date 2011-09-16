4060a5906e273bd0bb2a62ebf98795aa0b087533
Chris Kirkham <chrisjameskirkham@gmail.com>
2011-09-16 19:17:03 +0100

    Added a tool to take photos using the V4L2 parts of libkoki

---
+++ tools/take_photo.c
@@ -0,0 +1,74 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include <cv.h>
+#include <highgui.h>
+#include <linux/videodev2.h>
+
+#include "koki.h"
+
+#define WIDTH  640
+#define HEIGHT 480
+
+
+int main(int argc, const char **argv)
+{
+
+	int fd;
+	int num_buffers = 1;
+	struct v4l2_format fmt;
+	const char *dev;
+	const char *filename;
+	koki_buffer_t *buffers;
+	uint8_t *frame;
+	IplImage *output;
+
+	if (argc != 3){
+		printf("Usage: %s VIDEO_DEVICE FILENAME\n", argv[0]);
+		return 1;
+	}
+
+	dev = argv[1];
+	filename = argv[2];
+
+	fd = koki_v4l_open_cam(dev);
+
+	if (fd == -1){
+		printf("Couldn't open camera '%s'\n", dev);
+		return 1;
+	}
+
+	fmt = koki_v4l_create_YUYV_format(WIDTH, HEIGHT);
+	if (koki_v4l_set_format(fd, fmt) < 0){
+		printf("Unable to set format:\n");
+		koki_v4l_print_format(fmt);
+		return 1;
+	}
+
+	buffers = koki_v4l_prepare_buffers(fd, &num_buffers);
+
+	if (num_buffers < 1){
+		printf("Not enough buffer(s)\n");
+		return 1;
+	}
+
+	if (koki_v4l_start_stream(fd) < 0){
+		printf("Unable to start stream\n");
+		return 1;
+	}
+
+	frame = koki_v4l_get_frame_array(fd, buffers);
+	assert(frame != NULL);
+
+	output = koki_v4l_YUYV_frame_to_RGB_image(frame, WIDTH, HEIGHT);
+
+	cvSaveImage(filename, output, 0);
+
+	cvReleaseImage(&output);
+
+	koki_v4l_stop_stream(fd);
+	koki_v4l_close_cam(fd);
+
+	return 0;
+
+}
