d63d70336e59164313559f7cbdcd1700bd0602d8
Robert Spanton <rspanton@zepler.net>
2011-10-28 17:42:09 +0100

    License the project under GPLv3+

--- include/code_table.h
+++ include/code_table.h
@@ -1,3 +1,19 @@
+/* Copyright 2011 Chris Kirkham
+
+   This file is part of libkoki
+
+   libkoki is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   libkoki is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with libkoki.  If not, see <http://www.gnu.org/licenses/>. */
 /* AUTO GENERATED -- DO NOT MODIFY */
 static int fwd_code_table[256] = {
 	0,	1,	-1,	-1,	2,	3,	4,	5,
